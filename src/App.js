c733337670d6e19ee4d108029ac281013315748b
Bambara123 <e19034@eng.pdn.ac.lk>
2024-07-13 16:04:34 +0530

    meta tags and sematics updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Helmet } from "react-helmet";
+
+import Nav from "./components/Nav/Nav";
+import HeroSection from "./components/HeroSection/HeroSection";
+import SpecialDishes from "./components/SpecialDishes/SpecialDishes";
+import Testimonials from "./components/Testimonials/Testimonials";
+import AboutSection from "./components/AboutSection/AboutSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div className="App">Home page</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Little Lemon</title>
+        <meta
+          name="description"
+          content="Litte lemon is a restaurant that offers a variety of dishes including vegan and vegetarian options."
+        />
+        <meta name="og:image" content="images/greek salad.jpg" />
+        <meta
+          name="og:description"
+          content="itte lemon is a restaurant that offers a variety of dishes including vegan and vegetarian options."
+        />
+        <meta name="og:title" content="Little Lemon" />
+      </Helmet>
+      <Nav></Nav>
+      <HeroSection></HeroSection>
+      <SpecialDishes></SpecialDishes>
+      <Testimonials></Testimonials>
+      <AboutSection></AboutSection>
+      <Footer> </Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/SpecialDishes/SpecialDishes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SpecialDishes() {
+  return <div>SpecialDishes</div>;
+}
